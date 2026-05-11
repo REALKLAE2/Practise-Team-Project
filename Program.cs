5a94f9230bf781fa20aa7c051e33d45b8e3e4f78
Klae Eckhold (1000135705) <ECKHKL1@student.op.ac.nz>
2026-05-12 10:30:52 +1200

    Setting up project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practise_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
