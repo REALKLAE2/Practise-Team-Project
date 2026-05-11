bfdf287fedab05314cd2feb3ea335d056629ac1c
Klae Eckhold (1000135705) <ECKHKL1@student.op.ac.nz>
2026-05-12 10:47:31 +1200

    Added my part of my code from Lab 13

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Practise_Project
+﻿using System;
+
+namespace Practise_Project
 {
     internal class Program
     {
@@ -12,7 +14,21 @@
 
 
             //Klae
+            string phrase = "the quick brown fox jumps over the lazy dog";
+            string temp, newphrase;
+            Console.WriteLine("Enter THE PHRASE");
+            temp = Console.ReadLine();
+            newphrase = temp.TrimEnd();
+            string hairy = " hairy";
+            string finalphrase = newphrase.Replace("dog", "chicken");
+            Console.WriteLine(finalphrase);
+            Console.WriteLine(newphrase.Insert(10, hairy));
+            Console.ReadLine();
 
+            finalphrase = finalphrase.ToUpper();
+            Console.WriteLine(finalphrase);
+            finalphrase = finalphrase.ToLower();
+            Console.WriteLine(finalphrase);
 
             //Luke
 
