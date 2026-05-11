17ffe02770f5e3c9ad5197de334d38f55101362d
Stephanie Soldevilla (1000139106) <SOLDSO1@student.op.ac.nz>
2026-05-12 10:55:01 +1200

    Stephanie's modified code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Practise_Project
+﻿using System;
+
+namespace Practise_Project
 {
     internal class Program
     {
@@ -9,7 +11,35 @@
 
 
             //Stephanie
+            //Lab 13 Activity 4
+            int uppercase = 0;
+            int lowercase = 0;
+            int others = 0;
 
+            Console.WriteLine("Enter a line of text: ");
+            string input = Console.ReadLine();
+
+            foreach (char ch in input)
+            {
+                if (char.IsUpper(ch))
+                {
+                    uppercase++;
+                }
+                else if (char.IsLower(ch))
+                {
+                    lowercase++;
+                }
+                else
+                {
+                    others++;
+                }
+            }
+
+            Console.WriteLine("/nResults");
+            Console.WriteLine($"Uppercase letters: {uppercase}");
+            Console.WriteLine($"Lowercase letters: {lowercase}");
+            Console.WriteLine($"Other characters: {others}");
+            Console.ReadLine();
 
             //Klae
 
