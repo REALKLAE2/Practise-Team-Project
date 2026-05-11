90b569d8de39dc6b0d5f723c74ebb98f69f52f31
Juan Marcus Sumaya (1000136474) <SUMAJA1@student.op.ac.nz>
2026-05-12 10:45:12 +1200

    Marcus Commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Practise_Project
+﻿using System;
+
+namespace Practise_Project
 {
     internal class Program
     {
@@ -18,9 +20,30 @@
 
 
             //Juan
+            int[] counts = new int[7];
+            Random rng = new Random();
 
+            for (int rollNumber = 0; rollNumber < 6000; rollNumber++)
+            {
+                int result = rng.Next(1, 7);
+                counts[result]++;
+            }
+            Console.WriteLine("||=======================================||");
+            Console.WriteLine("||    Face    ||     Count    ||    %    ||");
+            Console.WriteLine("||=======================================||");
+
+            for (int face = 1; face <= 6; face++)
+            {
+                double percentage = (counts[face] / 6000.0) * 100;
+                Console.WriteLine($"||     {face}     ||   {counts[face],4}   ||    {percentage,6:F2}%   ||");
+            }
+            Console.WriteLine("||=======================================||");
+
+            Console.ReadLine();
+            //
 
             //Fletch
+
 
 
 
